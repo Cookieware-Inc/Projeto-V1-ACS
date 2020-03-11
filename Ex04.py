a600788ae3a4b8dde87bb19b0f7bf7c98e6d718f
EmmanoelCoutinho <lenerdgamer101@gmail.com>
2020-03-10 23:55:56 -0300

    v1

---
+++ Ex04.py
@@ -0,0 +1,20 @@
+print(' Oi, Meu nome é ACS')
+print(' Como Você está hoje?  bem | mal')
+r = input(' Resposta : ')
+
+
+if r == 'bem':
+    print(' Que bom!Vamos Conversar?')
+
+if r == 'mal':
+    print(" Poxa! Conte o que houve!")
+
+
+r2 = input(' Resposta: ')
+
+if r2 == 'vamos':
+    print('Quantos anos você tem?')
+else:
+    print('Okay, ;-; vamos falar sobre o que você gosta?')
+
+r3 = input('Diga lá meu chefe! : ')
